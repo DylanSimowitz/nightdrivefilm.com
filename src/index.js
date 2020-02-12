69a2c8e56e80be33f938b3794190768734d8e6c8
Dylan Simowitz <git@dylan.simowitz.com>
2020-02-12 00:42:20 -0800

    live

--- src/index.js
+++ src/index.js
@@ -3,6 +3,25 @@
 import "./css/main.css";
 import ScrollReveal from "scrollreveal";
 
+function ready(fn) {
+  if (document.readyState != "loading") {
+    fn();
+  } else {
+    document.addEventListener("DOMContentLoaded", fn);
+  }
+}
+
 ScrollReveal().reveal(".reveal", { interval: 400 });
 ScrollReveal().reveal(".section-heading", { orign: "left", distance: "20px" });
 ScrollReveal().reveal(".reveal-credit", { interval: 100 });
+
+ready(() => {
+  const $menuButton = document.querySelector("#menu-button");
+  const $menu = document.querySelector("#menu");
+  $menuButton.addEventListener("click", () => {
+    $menu.classList.toggle("dn-important");
+  });
+  $menu.addEventListener("click", () => {
+    $menu.classList.toggle("dn-important");
+  });
+});
